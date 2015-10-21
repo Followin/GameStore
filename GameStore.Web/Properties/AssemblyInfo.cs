6c731dd7676c30f822d55276a75dd6fdc00c20eb
Pavlo Yeremenko <pavlo_yeremenko@epam.com>
2015-10-21 13:04:18 +0300

    Stirng.Empty errors resolved

--- GameStore.Web/Properties/AssemblyInfo.cs
+++ GameStore.Web/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GameStore.Web")]
-[assembly: AssemblyDescription(String.Empty)]
-[assembly: AssemblyConfiguration(String.Empty)]
-[assembly: AssemblyCompany(String.Empty)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GameStore.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark(String.Empty)]
-[assembly: AssemblyCulture(String.Empty)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 

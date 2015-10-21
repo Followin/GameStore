c9eb96df2d5545549d6c3bff67fd4651a098148b
Pavlo Yeremenko <pavlo_yeremenko@epam.com>
2015-10-21 12:02:25 +0300

    Last test commit

--- GameStore.Tests/BLLTests/GameTests.cs
+++ GameStore.Tests/BLLTests/GameTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Security.Cryptography.X509Certificates;
 using GameStore.BLL.CommandHandlers;
 using GameStore.BLL.Commands;
 using GameStore.BLL.Queries;

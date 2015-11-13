c084857bfd9a4e1c89c4dec0a484256911ad42a6
Pavlo Yeremenko <pavlo_yeremenko@epam.com>
2015-11-13 21:39:40 +0200

    Useless

--- GameStore/GameStore.Web/Models/Game/CreateGameViewModel.cs
+++ GameStore/GameStore.Web/Models/Game/CreateGameViewModel.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 using GameStore.Web.App_LocalResources;
 using GameStore.Web.Models.Genres;
-using Newtonsoft.Json;
 
 namespace GameStore.Web.Models.Game
 {
